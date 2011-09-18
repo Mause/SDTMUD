ea5532cd5854c82765fd8138706c95d74e68c9ab
Jack.thatch@gmail.com <Jack.thatch@gmail.com@77f1e1fb-88bf-2743-3d0c-9ff6485b97db>
2011-09-18 12:51:28 +0000

    git-svn-id: https://dtmud.googlecode.com/svn@3 77f1e1fb-88bf-2743-3d0c-9ff6485b97db

--- trunk/mud_mud/East/East1.py
+++ trunk/mud_mud/East/East1.py
@@ -1,3 +1,7 @@
 def run_room(clientsocket, instance):
-    clientsocket.send('You are standing on a crossroads, your life depends on this')
+    clientsocket.send('''You are standing on a crossroads, whether or not you continue to live depends on a desition made here!
+Viable commands: north, south\n
+                      ''')
+    cur_com = clientsocket.recv(1024)
+    #if 
     return 'Misc.endroom'
