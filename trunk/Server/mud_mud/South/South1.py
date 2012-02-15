c794070c2af2f6da610c3d68f67fd5749fa1fb85
jack.thatch@gmail.com <jack.thatch@gmail.com@77f1e1fb-88bf-2743-3d0c-9ff6485b97db>
2012-02-15 13:23:10 +0000

    git-svn-id: https://dtmud.googlecode.com/svn@14 77f1e1fb-88bf-2743-3d0c-9ff6485b97db

---
+++ trunk/Server/mud_mud/South/South1.py
@@ -0,0 +1,15 @@
+print 'Directly to your south is a theater, which appears to have ghosts circling around it'
+print 'To the east and west run the road which you are standing on.'
+print 'To the east there are some banners hovering in the distance, over a hill.'
+print 'To the west, there is a castle with coloured lights over it.'
+print 'viable commands: south, north, east, west, rest, jump.'
+South1 = raw_input()
+if South1 == rest
+    health = 100
+if South1 == jump
+    print 'You jump on the spot.'
+    print 'At the top of your jump, an arrow from the banners hits you in the face.'
+    print 'You now have 30% health'
+    health = 30
+if South1 == south
+    alex_open('theater.py')
